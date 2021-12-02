d6ffbe8ea7bab408adc82a645f447cd18e83eb96
Vetlebrur <vetlerod@gmail.com>
2021-12-02 12:09:05 +0100

    Update Silisium_Beta.js

--- Silisium_Beta/src/Silisium_Beta.js
+++ Silisium_Beta/src/Silisium_Beta.js
@@ -19,12 +19,19 @@ function calculate(){
         outputEl.innerHTML = `${a} - ${b} = ${a-b}`;
 
     }
-    else if(equation.includes("*")){
+    else if(equation.includes("*")|| equation.includes("^")){
         if (equation.includes("**")){
             let symbolLocation = equation.indexOf("*");
             let a = Number(equation.slice(0,symbolLocation));
             let b = Number(equation.slice(symbolLocation+2,equationLength))
             outputEl.innerHTML = `${a} ^ ${b} = ${a**b}`;
+        }
+        else if(equation.includes("^")){
+            let symbolLocation = equation.indexOf("^");
+            let a = Number(equation.slice(0,symbolLocation));
+            let b = Number(equation.slice(symbolLocation+1,equationLength))
+            outputEl.innerHTML = `${a} ^ ${b} = ${a**b}`;
+
         }
         else{
             let symbolLocation = equation.indexOf("*");
