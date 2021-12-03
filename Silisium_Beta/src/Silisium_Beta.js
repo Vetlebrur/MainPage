05f0c920020b754f7f7ec64540a47397030fedc5
Vetlebrur <vetlerod@gmail.com>
2021-12-03 12:27:09 +0100

    Update Silisium_Beta.js

--- Silisium_Beta/src/Silisium_Beta.js
+++ Silisium_Beta/src/Silisium_Beta.js
@@ -2,9 +2,8 @@ inputEl = document.querySelector("#formelInput");
 btnEl = document.querySelector("#button");
 outputEl = document.querySelector("#formelOutput");
 
-
 function calculate(){
-    let equation = inputEl.value.replaceAll(" ","").toLowerCase();
+    let equation = inputEl.value.replaceAll(" ","").replaceAll("=","").toLowerCase();
     if(equation == ""){
         alert("insert values");
         return;
@@ -16,9 +15,15 @@ function calculate(){
     }
     var newEquation = equation.replaceAll("one",1).replaceAll("two",2).replaceAll("three",3).replaceAll("four",4)
     .replaceAll("five",5).replaceAll("six",6).replaceAll("seven",7).replaceAll("eight",8).replaceAll("nine",9).replaceAll("ten",10)
-    .replaceAll("^","**").replaceAll("plus","+").replaceAll("times","*").replaceAll("power","**");
+    .replaceAll("^","**").replaceAll("plus","+").replaceAll("times","*").replaceAll("power","**").replaceAll("sqrt","Math.sqrt");
+
+    try {
+        outputEl.innerHTML = `${newEquation.replaceAll("**","^")} = ${eval(newEquation)}`;
+    }
+    catch{
+        alert("error");
+    }
     
-    outputEl.innerHTML = `${newEquation.replaceAll("**","^")} = ${eval(newEquation)}`;
 }
 
 
